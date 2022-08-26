4eab7de65de9f328aa6ad5db3e8c574d718c43f0
erickaltamirano07 <79813692+erickaltamirano07@users.noreply.github.com>
2022-08-25 19:10:32 -0500

    archivos y juego del ahorcado

--- filtrando_datos_challenge.py
+++ filtrando_datos_challenge.py
@@ -74,10 +74,22 @@ DATA = [
 def run():
 
     # Comprehensions solutions
-    all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
-    all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
-    adults =  [worker["name"] for worker in DATA if worker["age"] > 18]
-    old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
+    # all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
+    # all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+    # adults =  [worker["name"] for worker in DATA if worker["age"] > 18]
+    # old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
+
+    # Challenge completed
+    all_python_devs = list(filter(lambda worker: worker["language"] == "python", DATA))
+    all_python_devs = list(map(lambda worker: worker["name"], all_python_devs))
+    
+    all_Platzi_workers = list(filter(lambda worker: worker["organization"] == "Platzi", DATA))
+    all_Platzi_workers = list(map(lambda worker: worker["name"], all_Platzi_workers))
+
+    adults = list(filter(lambda worker: worker["age"] > 18, DATA))
+    adults = list(map(lambda worker: worker["name"], adults))
+
+    old_people = [worker | {"old": worker["age"] > 70} for worker in DATA]
 
     for worker in all_python_devs:
         print(worker)
